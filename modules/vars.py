efdfab79b3d9ade254ea8651d49f5ccd68e8f393
imad5445 <157965306+imad5445@users.noreply.github.com>
2024-02-15 12:21:18 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "28088533"
 api_hash = "05533c10eb18ff30df25e3b7f77d49e6"
-bot_token = "6856044634:AAG3INgQ6YITyH7Ooa5WinkO3VneqPLQOXk"
+bot_token = "6968646457:AAFAqLgWFb4VoH7sI0Be3j2A5tj_My-RV_o"
